4e9434493515f536848017d21f2c2b30fafb2419
GroundLaw <jojungon@gmail.com>
2025-06-09 17:30:15 +0900

    [FEAT] tui 차트 생성 test

---
+++ src/main/java/com/utilmodel/restController/ToastUIAPIController.java
@@ -0,0 +1,18 @@
+//package com.utilmodel.restController;
+//
+//import com.utilmodel.service.ToastUIService_IF;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class ToastUIAPIController {
+//    @Autowired
+//    private ToastUIService_IF toastUIService;
+//
+//    @GetMapping("/")
+//    public String toastUI(Model model) {
+//
+//    }
+//}
